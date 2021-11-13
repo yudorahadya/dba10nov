d8aeee544c920a2222d5143c86a5b9f8ad63891a
yudorahadya <yudo.rahadya@gmail.com>
2021-11-13 10:43:34 +0700

    edit sql

--- ddl/mysql.sql
+++ ddl/mysql.sql
@@ -1,38 +1,49 @@
-create table categories (
-    category_id integer(100) not null auto_increment,
-    category_name varchar(100),
-    description varchar(100),
-    picture text,
-    primary key (category_id)
-);
+-- mutualfund_product.categories definition
 
-create table products (
-    product_id integer(100) not null auto_increment,
-    product_name varchar(100),
-    supplier_id integer(100),
-    category_id integer(100),
-    quantity_per_unit varchar(100),
-    unit_price integer(100),
-    units_in_stock integer(100),
-    units_on_order integer(100),
-    reorder_level integer(100),
-    discontinued integer(100),
-    primary key (product_id)
-);
+CREATE TABLE `categories` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `picture` text,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table suppliers (
-    supplier_id integer(100) not null auto_increment,
-    company_name varchar(100),
-    contact_name varchar(100),
-    contact_title varchar(100),
-    address varchar(100),
-    city varchar(100),
-    region varchar(100),
-    postal_code varchar(100),
-    country varchar(100),
-    phone varchar(100),
-    fax varchar(100),
-    homepage varchar(100),
-    primary key (supplier_id)
-);
 
+-- mutualfund_product.suppliers definition
+
+CREATE TABLE `suppliers` (
+  `supplier_id` int NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(100) DEFAULT NULL,
+  `contact_name` varchar(100) DEFAULT NULL,
+  `contact_title` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `region` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `fax` varchar(100) DEFAULT NULL,
+  `homepage` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`supplier_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- mutualfund_product.products definition
+
+CREATE TABLE `products` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) DEFAULT NULL,
+  `supplier_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `quantity_per_unit` varchar(100) DEFAULT NULL,
+  `unit_price` int DEFAULT NULL,
+  `units_in_stock` int DEFAULT NULL,
+  `units_on_order` int DEFAULT NULL,
+  `reorder_level` int DEFAULT NULL,
+  `discontinued` int DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `products_FK` (`category_id`),
+  KEY `products_FK_1` (`supplier_id`),
+  CONSTRAINT `products_FK` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
+  CONSTRAINT `products_FK_1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`supplier_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
